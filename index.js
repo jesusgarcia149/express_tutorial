531d47f87043ef7932e09e78cb01ff0ab2e05dba
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-10 11:45:36 -0400

    .

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/about', (req, res) => {
+    res.send('About');
+})
+
+app.get('/Weather', (req, res) => {
+    res.send('Weather');
+})
+
+app.use((req, res) => {
+    res.status(404).send('page not found');
+})
+
 app.listen(3000);
 console.log('Server on Port 3000');
